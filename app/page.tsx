bd0fe18d04453572a972b8c1aa4e6360ace71f4c
olumidejoseph2002-debug <olumidejoseph2002@gmail.com>
2025-11-13 07:27:28 -0800

    new page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const page = () => {
   return (
     <header className='bg-black-500'>
-      
+     <img src="../public/image/image" alt="" />
+
+     new page
     </header>
   )
 }
