013e6594003145efacd6ab852b8e2b977bbdaa99
olumidejoseph2002-debug <olumidejoseph2002@gmail.com>
2025-11-22 23:38:42 -0800

    Almost done

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Contacts = () => {
   return (
@@ -10,7 +11,7 @@ const Contacts = () => {
       
 
 
-    <div className='flex w-full items-start justify-between gap-5 mt-12 '>
+    <div className='flex w-full items-start  gap-5 mt-12 '>
           <div >
             <div className='text-gray-400 text-2xl'>
              Let&apos;s
@@ -30,18 +31,87 @@ const Contacts = () => {
             <div className='mt-10'>
               Follow
               </div>
+              <div className='flex flex-row gap-7 mt-4'>
+                      <Image
+                    src="/icon/instagram.svg"
+                        alt=""
+                        width={16}
+                        height={16}
+                        className="flex-shrink-0 rounded-[40px]" />
+              
+                        <Image
+                    src="/icon/facebook.svg"
+                        alt=""
+                        width={16}
+                        height={16}
+                        className="flex-shrink-0 rounded-[40px]" />
+              
+                        <Image
+                    src="/icon/twitter.svg"
+                        alt=""
+                        width={16}
+                        height={16}
+                        className="flex-shrink-0 rounded-[40px]" />
+                    </div>
           </div>
 
-
-    <div className='text-red-400 text-xl ml-20'>
+    <div className='flex flex-col items-start ml-20 gap-6 '>
+    <div className='text-red-400 text-xl '>
       Talk
     </div>
-    </div>
+    <div className='w-[800px] h-[624px] bg-gray-300 rounded-2xl flex flex-col p-10 gap-4 '>
 
-    <div>
-    <div>We do not tell you our story.</div>
-    <div>We write it together.</div>
-    <div>Partnering with us means a seat at the table where you will be heard.</div>
+      <Image
+      src="/image/message.png"
+          alt=""
+          width={426}
+          height={44}
+          className="flex-shrink-0 rounded-lg mt-2"
+          />
+      
+      <div>Name</div>
+      <div className='w-180 h-10 bg-white rounded-lg'></div>
+
+      <div className='flex flex-row gap-6'>
+      <div>
+        Email
+        <div className='w-87 h-10 bg-white rounded-lg mt-5'></div>
+      </div>
+      
+
+      <div>
+        Phone
+        <div className='w-87 h-10 bg-white rounded-lg mt-5'></div>
+        </div>
+      
+      </div>
+
+      <div>Your message</div>
+      <div className='w-180 h-40 bg-white rounded-lg'></div>
+
+      <Image
+      src="/image/send.png"
+          alt=""
+          width={113}
+          height={48}
+          className="flex-shrink-0 rounded-lg mt-6 mx-75"
+    />
+    
+    </div>
+    </div>
+      </div>
+
+    <div className=' w-full flex flex-col items-center text-center text-bold mt-20 mb-20'>
+    <div className='font-semibold text-[30px]'>We do not tell you our story.</div>
+    <div className='font-semibold text-[30px]'>We write it together.</div>
+    <div className='text-[18px] text-[#808080] mt-6'>Partnering with us means a seat at the table where you will be heard.</div>
+    <Image
+      src="/image/Sproject.png"
+          alt=""
+          width={175}
+          height={48}
+          className="flex-shrink-0 rounded-lg mt-6"
+    />
     </div>
      </div>
     </>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,23 +4,51 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <>
-    <div className='flex w-full items-start justify-between gap-8 mt-12'>
+    <div className='flex w-full items-start justify-between gap-8 mt-12 px-36'>
     <div>
-      <Image />
+      <Image
+      src="/icon/logo.svg"
+          alt=""
+          width={91}
+          height={26}
+          className="flex-shrink-0 rounded-[40px]" />
 
-     <div>At MediaFlare, we have developed a simple, media-neutral approach that will handle all the planning, production, distribution, amplification and measurement of your content so we can continuously improve your results.</div>
+     <div className='w-[350px] leading-snug mt-4 text-[14px] text-[#808080]'>At MediaFlare, we have developed a simple, media-neutral approach that will handle all the planning, production, distribution, amplification and measurement of your content so we can continuously improve your results.</div>
     </div>
 
-    <div>
-      <div>Quick Information</div>
-      <div>OUR SERVICES</div>
+    <div className='ml-90'>
+      <div className='text-[16px] text-[#808080]'>Quick Information</div>
+      <div className='text-[141414] font-medium mt-4'>OUR SERVICES
       <div>OUR TEAM</div>
       <div>OUR WORK</div>
       <div>CONTACT US</div>
     </div>
+    </div>
 
     <div>
-      <div>Follow</div>
+      <div className='text-[16px] text-[#808080]'>Follow</div>
+      <div className='flex flex-row gap-7 mt-4'>
+        <Image
+      src="/icon/instagram.svg"
+          alt=""
+          width={16}
+          height={16}
+          className="flex-shrink-0 rounded-[40px]" />
+
+          <Image
+      src="/icon/facebook.svg"
+          alt=""
+          width={16}
+          height={16}
+          className="flex-shrink-0 rounded-[40px]" />
+
+          <Image
+      src="/icon/twitter.svg"
+          alt=""
+          width={16}
+          height={16}
+          className="flex-shrink-0 rounded-[40px]" />
+      </div>
     </div>
 
     </div>
